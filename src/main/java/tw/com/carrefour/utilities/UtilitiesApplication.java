03b435ccb3e80f980944b360414441a491d69f7c
KK <kktry57@gmail.com>
2020-11-23 10:47:23 +0800

    correct proj

--- src/main/java/tw/com/carrefour/utilities/UtilitiesApplication.java
+++ src/main/java/tw/com/carrefour/utilities/UtilitiesApplication.java
@@ -6,8 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class UtilitiesApplication {
 
-	public static void main(String[] args) {
-		System.out.println("12345".sub(0, 2));
+	public static void main(String[] args) {		
 		System.out.println(MyClass.hello());
 
 		
